b8832e09894f2fbd742f69a499b25b042dc4754a
Keshavinee <shrikeshavinee@gmail.com>
2021-02-28 14:55:40 +0530

    Symbolic Constants

--- Chapter-1/ex5.c
+++ Chapter-1/ex5.c
@@ -2,11 +2,17 @@
 
 #include <stdio.h>
 
+/* Symbolic Constants */
+
+#define max 300
+#define min 0
+#define decrement 20
+
 int main()
 {
     float celsius;
 
-    for (celsius=300;celsius>=0;celsius=celsius-20)
+    for (celsius=max;celsius>=min;celsius=celsius-decrement)
         printf("%3.0f\t%6.1f\n",celsius,9.0/5.0*celsius+32);
 
 }
