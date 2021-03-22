1973b858c67927e37d617b9fec6482966cf89edb
Keshavinee <shrikeshavinee@gmail.com>
2021-03-22 10:40:53 +0530

    counts blanks, tabs and newlines

--- Chapter-1/ex9.c
+++ Chapter-1/ex9.c
@@ -1,14 +1,22 @@
+/* This program counts blanks, tabs and newlines. */
+
 #include<stdio.h>
 
 int main()
 {
-    int c,line_count;
-    line_count=0;
+    int c,blank,tab,newline;
+    blank=tab=newline=0;
 
     while ((c=getchar())!=EOF)
     {
-        if (c == '\n')
-            ++line_count;
+        if (c == ' ')
+            ++blank;
+        else if (c=='\t')
+            ++tab;
+        else if (c=='\n')
+            ++newline;
     }
-    printf("No.of lines: %d\n",line_count);
+    printf("No.of blanks: %d\n",blank);
+    printf("No.of tabs: %d\n",tab);
+    printf("No.of newlines: %d\n",newline);
 }
